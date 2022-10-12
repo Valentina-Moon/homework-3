cda04826037fb0d989ad7de24ec2563fda049503
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-12 18:19:52 +0300

    произведение чисел от 1 до N

---
+++ task№3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите число N:");
+int N =  int.Parse(Console.ReadLine() ?? "0");
+int P = 1;
+int i; 
+for (i = 1; i <= N; i++)
+{ 
+            P = P* i;
+} 
+Console.WriteLine($"произведение чисел равно {P}"); 
