54417bbfb12b31dd3f481354fced8e7be3c5ffab
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-12 16:55:58 +0300

    сумма цифр в числе

---
+++ task№2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Подсчитать сумму цифр в числе
+Console.WriteLine("Введите число:");
+int A = int.Parse(Console.ReadLine()?? "0");
+int S = 0;
+while (A > 0)
+{
+
+    S = S + A % 10;
+    A = A / 10;
+
+}
+Console.WriteLine($"сумма числа равна {S}");
