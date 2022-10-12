125deaf1609de6f51f105d8e86c33d16bed6cdc1
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-12 19:08:19 +0300

    кубы чисел, заканчивающихся на четную цифру

---
+++ task№4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите число A:");
+int A = int.Parse(Console.ReadLine() ?? "0");
+while (A < N)
+{
+    int S = A * A * A;
+    
+    if (S%2 == 0)
+    Console.WriteLine($"A = {A}, куб числа A заканчивающийся на четную цифру  = {S}");
+    A++;
+}
+
+
+
+
