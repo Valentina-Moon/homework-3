cf8cfeea6ec6b907b14c8e7b398f75aae7456873
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-12 16:16:52 +0300

    куб числа от 1 до N

---
+++ task№1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int A = 1;
+while (A < N)
+{
+    int S = A * A * A;
+    Console.WriteLine($"A = {A}, куб числа A = {S}");
+    A++;
+}
